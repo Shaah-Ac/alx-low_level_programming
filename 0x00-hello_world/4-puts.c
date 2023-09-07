3bdb3cc8b38d5ed6a4ad25ce43d07aab28740ea5
Sharon Achieng <othienosharon51@gmail.com>
2023-09-07 13:28:16 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
 /**
- *  main - this is the main put function
+ *  main - Entry point
  * 
- * Return: 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"programmimg is like building a multilingual puzzle");
+	puts("\"Programmimg is like building a multilingual puzzle");
 	return (0);
 }
 
