f8b845355a513b379e874b002bc43db7899dc780
Sharon Achieng <otienosharon51@gmail.com>
2023-10-24 05:37:00 -0700

    task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-#include <stdio.h>
+
 /**
  * free_listint_safe - frees a linked list
  * @h: pointer to the first node in the linked list
@@ -9,20 +9,30 @@
 size_t free_listint_safe(listint_t **h)
 {
 	size_t len = 0;
+	int diff;
 	listint_t *temp;
 
-		if (!h || !*h)
-		return (0);
+	if (!h || !*h)
+	return (0);
 
-		while (*h)
-		{
-			temp = (*h)->next;
-			*h = temp;
-			len++;
-		}
-
-
-
+	while (*h)
+	{
+		diff = *h - (*h)->next;
+		if (diff > 0)
+	{
+		temp = (*h)->next;
+		free(*h);
+		*h = temp;
+		len++;
+	}
+	else
+	{
+		free(*h);
+		*h = NULL;
+		len++;
+		break;
+	}
+	}
 
 	*h = NULL;
 
