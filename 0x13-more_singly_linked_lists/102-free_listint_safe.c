977e7ede407ae6f83b715a3da590f9d65870e63c
Sharon Achieng <otienosharon51@gmail.com>
2023-10-24 02:14:38 -0700

    task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -9,28 +9,20 @@
 size_t free_listint_safe(listint_t **h)
 {
 	size_t len = 0;
-	int diff;
 	listint_t *temp;
 
-	if (!h || !*h)
+		if (!h || !*h)
 		return (0);
 
-	while (*h)
-	{
-		diff = *h - (*h)->next;
-		if (diff > 0)
+		while (*h)
 		{
 			temp = (*h)->next;
 			*h = temp;
 			len++;
 		}
-		else
-		{
-			*h = NULL;
-			len++;
-			break;
-		}
-	}
+
+
+
 
 	*h = NULL;
 
