8eb127a0660f871544905f8348e78dc0b7eaf93f
Sharon Achieng <otienosharon51@gmail.com>
2023-10-26 13:44:32 -0700

    bit manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,13 +7,13 @@
  *
  * Return: 1 if success, or -1 if an error occurred
  */
-int set_bit(unsigned long int *m, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int setbit;
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
 	setbit = 1 << index;
-	*m = *m | setbit;
+	*n = *n | setbit;
 	return (1);
 }
